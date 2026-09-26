bb6aecf6f6004c81900c8f560483080b837d45b1
mainulislamrabby <mdmainulislamrabby4@gmail.com>
2026-09-26 09:54:48 +0600

    Added Workout Card and Workout Types

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
       lang="en"
+      data-theme="dark"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="bg-[#0c0d10] text-white h-full">
